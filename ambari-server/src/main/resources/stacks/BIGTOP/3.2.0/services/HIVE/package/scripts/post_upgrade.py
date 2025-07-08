361cb5346b1ac3bea24946ba249782931ebb4ce8
huadi <292801371@qq.com>
2025-07-08 20:32:10 +0800

    feat: 适配os，bigtop 改为 hdp

--- ambari-server/src/main/resources/stacks/BIGTOP/3.2.0/services/HIVE/package/scripts/post_upgrade.py
+++ ambari-server/src/main/resources/stacks/BIGTOP/3.2.0/services/HIVE/package/scripts/post_upgrade.py
@@ -45,7 +45,7 @@ class HivePostUpgrade(Script):
 
     target_version = upgrade_summary.get_target_version(service_name="HIVE")
 
-    hive_script = format("/usr/bigtop/{target_version}/usr/lib/hive/bin/hive")
+    hive_script = format("/usr/hdp/{target_version}/usr/lib/hive/bin/hive")
     cmd = format(
       "{hive_script} --config /etc/hive/conf --service  strictmanagedmigration --hiveconf hive.strict.managed.tables=true  -m automatic  --modifyManagedTables --oldWarehouseRoot /apps/hive/warehouse"
     )
